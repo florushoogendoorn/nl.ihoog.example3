6cbef45b0206e33a0abbe6e4837ad5c552013a7e
Florus Hoogendoorn <florus@ihoog.nl>
2016-11-24 06:38:55 +0100

    Add sensor

--- app.js
+++ app.js
@@ -5,7 +5,31 @@ var http = require('http');
 module.exports.init = function() {
 
     setInterval(SaveTemp, 5 * 60 * 1000);
+    
+/*
+    Homey.manager('insights').deleteLog('sens2measure' , function callback(err, success){
+        if( err ) return console.error(err);
+    });
+    
+    Homey.manager('insights').deleteLog('sens3measure' , function callback(err, success){
+        if( err ) return console.error(err);
+    });
+    
+	If (Homey.manager('settings').get( 'name1' ) != "") { InitTemp(1); }
+	If (Homey.manager('settings').get( 'name2' ) != "") { InitTemp(2); }
+	If (Homey.manager('settings').get( 'name3' ) != "") { InitTemp(3); }
 
+	InitTemp(2);
+
+*/
+
+	// Get all logs, made by this app
+	Homey.manager('insights').getLogs(
+		function callback(err , success){
+	    	if( err ) return console.error(err);
+	    	console.log(success);
+		});
+	
     Homey.manager('speech-input').on('speech', function(speech, callback) {
         getGardenTemp();
     });   
@@ -19,24 +43,52 @@ Homey.manager('flow').on('action.tell_garden_temp', function( callback, args ) {
 });
 
 
-function LogTemp(t) {
+function InitTemp(s) {
 
-	Homey.manager('insights').createLog( 'sensor1_measure', {
-	    label: { en: 'Temperatuur' },
+	var codeSens = "", nameSens = "", insName = "";
+	
+	switch(s) {
+    case 2:
+    	insName = 'sensor2_measure' //'sens2measure';
+		codeSens = Homey.manager('settings').get( 'code2' );
+		nameSens = Homey.manager('settings').get( 'name2' );
+        break;
+    case 3:
+    	insName = 'sens3measure';
+		codeSens = Homey.manager('settings').get( 'code3' );
+		nameSens = Homey.manager('settings').get( 'name3' );
+        break;
+    default:
+    	insName = 'sens1measure';
+		codeSens = Homey.manager('settings').get( 'code1' );
+		nameSens = Homey.manager('settings').get( 'name1' );
+	}
+
+
+	Homey.manager('insights').createLog( insName, {
+	    label: { en: nameSens },
 	    type: 'number',
 	    units: { en: '&deg;C' },
 	    decimals: 2,
 	    chart: 'line' 
 	}, function callback(err , success){
 	    if( err ) return console.error(err);
-	    Homey.manager('insights').createEntry( 'sensor1_measure', parseFloat(t), new Date(), function(err, success){
-		if( err ) return console.error(err);
-	    })
+//	    Homey.manager('insights').createEntry( insName, parseFloat('18')+s, new Date(), function(err, success){
+//			if( err ) return console.error(err);
+//	    })
 	});
 	
 }
 
 function SaveTemp() {
+
+	SaveTemp1();
+	SaveTemp2();
+	
+}
+
+
+function SaveTemp1() {
 
 	var apiurl = Homey.manager('settings').get( 'apiurl' );
 	var code1 = Homey.manager('settings').get( 'code1' );
@@ -61,7 +113,7 @@ function SaveTemp() {
     	Homey.manager('insights').createEntry( 'sensor1_measure', parseFloat(t) , new Date(), function(err, success){
         	if( err ) return console.error(err); 
         });     	
-     	
+     	     	
     });
             
     }).on('error', function(e)
@@ -69,6 +121,44 @@ function SaveTemp() {
         console.log("Got error: " + e.message);
     });
 }
+
+
+function SaveTemp2() {
+
+	var apiurl = Homey.manager('settings').get( 'apiurl' );
+	var code2 = Homey.manager('settings').get( 'code2' );
+
+    http.get(apiurl + code2, function(res) {
+        var body = '';
+		
+        res
+            .on('data', function(chunk)
+            {
+                body += chunk;
+            })
+            .on('end', function()
+            {
+                body = JSON.parse(body);
+		
+		var t = body.temperatuur;
+		var h = body.vochtigheid;
+
+     	console.log("createEntry t=" + parseFloat(t) + " new Date=" + new Date());
+     	
+    	Homey.manager('insights').createEntry( 'sensor2_measure', parseFloat(t) , new Date(), function(err, success){
+        	if( err ) return console.error(err); 
+        });     	
+     	     	
+    });
+            
+    }).on('error', function(e)
+    {
+        console.log("Got error: " + e.message);
+    });
+}
+
+
+
 
 
 function getGardenTemp() {
